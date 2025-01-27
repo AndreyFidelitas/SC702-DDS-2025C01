f0741f9f535c74ca7ac2045f4642ab0c25e0b59a
andreyszccr <andreyszcr@gmail.com>
2025-01-27 14:51:32 -0600

    Clase de camiones y formulario de Camiones

---
+++ CapaEntidades/Camiones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Camiones
+    {
+        public int CamionID { get; set; }
+        public string CamionCode { get; set; }
+        public string CamionName { get; set; }
+        public DateTime CamionCreacion { get; set; }
+        public DateTime CamionUpdate { get; set; }
+        public DateTime CamionDelete { get; set; }
+        public bool CamionStatus { get; set; }
+    }
+}
