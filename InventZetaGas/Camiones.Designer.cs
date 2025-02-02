3e6a83ee8b565b3ec6afa5105b9f44dbbd473dd2
andyszccr <andreyszcr@gmail.com>
2025-02-01 23:27:10 -0600

    Capa Negocios de Camiones
    Diseño de Camiones
    
    Falta ajustar el filtro y hacer pruebas de camiones

--- InventZetaGas/Camiones.Designer.cs
+++ InventZetaGas/Camiones.Designer.cs
@@ -61,10 +61,10 @@
             label2.Cursor = Cursors.Hand;
             label2.Font = new Font("Microsoft Sans Serif", 12F);
             label2.ForeColor = Color.DimGray;
-            label2.Location = new Point(12, 9);
+            label2.Location = new Point(14, 12);
             label2.Name = "label2";
             label2.Padding = new Padding(3, 0, 3, 0);
-            label2.Size = new Size(26, 20);
+            label2.Size = new Size(32, 25);
             label2.TabIndex = 2;
             label2.Text = "X";
             // 
@@ -73,9 +73,9 @@
             label1.AutoSize = true;
             label1.Font = new Font("Microsoft Sans Serif", 15F);
             label1.ForeColor = SystemColors.GrayText;
-            label1.Location = new Point(442, 9);
+            label1.Location = new Point(505, 12);
             label1.Name = "label1";
-            label1.Size = new Size(171, 25);
+            label1.Size = new Size(218, 29);
             label1.TabIndex = 3;
             label1.Text = "Modulo Camiones";
             // 
@@ -93,9 +93,11 @@
             gbRaza.Controls.Add(txtCodeCamion);
             gbRaza.Controls.Add(lblCode);
             gbRaza.Font = new Font("Microsoft Sans Serif", 9.75F);
-            gbRaza.Location = new Point(12, 37);
+            gbRaza.Location = new Point(14, 49);
+            gbRaza.Margin = new Padding(3, 4, 3, 4);
             gbRaza.Name = "gbRaza";
-            gbRaza.Size = new Size(905, 400);
+            gbRaza.Padding = new Padding(3, 4, 3, 4);
+            gbRaza.Size = new Size(1034, 533);
             gbRaza.TabIndex = 4;
             gbRaza.TabStop = false;
             gbRaza.Text = "Datos de Zonas";
@@ -103,36 +105,38 @@
             // txtPesaje
             // 
             txtPesaje.Font = new Font("Microsoft Sans Serif", 9.75F);
-            txtPesaje.Location = new Point(41, 277);
+            txtPesaje.Location = new Point(47, 369);
+            txtPesaje.Margin = new Padding(3, 4, 3, 4);
             txtPesaje.Name = "txtPesaje";
-            txtPesaje.Size = new Size(289, 22);
+            txtPesaje.Size = new Size(330, 26);
             txtPesaje.TabIndex = 12;
             // 
             // lblPesaje
             // 
             lblPesaje.AutoSize = true;
             lblPesaje.Font = new Font("Microsoft Sans Serif", 10F);
-            lblPesaje.Location = new Point(41, 248);
+            lblPesaje.Location = new Point(47, 331);
             lblPesaje.Name = "lblPesaje";
-            lblPesaje.Size = new Size(51, 17);
+            lblPesaje.Size = new Size(60, 20);
             lblPesaje.TabIndex = 11;
             lblPesaje.Text = "Pesaje";
             // 
             // txtPlaca
             // 
             txtPlaca.Font = new Font("Microsoft Sans Serif", 9.75F);
-            txtPlaca.Location = new Point(41, 210);
+            txtPlaca.Location = new Point(47, 280);
+            txtPlaca.Margin = new Padding(3, 4, 3, 4);
             txtPlaca.Name = "txtPlaca";
-            txtPlaca.Size = new Size(289, 22);
+            txtPlaca.Size = new Size(330, 26);
             txtPlaca.TabIndex = 10;
             // 
             // lblPlaca
             // 
             lblPlaca.AutoSize = true;
             lblPlaca.Font = new Font("Microsoft Sans Serif", 10F);
-            lblPlaca.Location = new Point(41, 179);
+            lblPlaca.Location = new Point(47, 239);
             lblPlaca.Name = "lblPlaca";
-            lblPlaca.Size = new Size(43, 17);
+            lblPlaca.Size = new Size(51, 20);
             lblPlaca.TabIndex = 9;
             lblPlaca.Text = "Placa";
             // 
@@ -140,9 +144,11 @@
             // 
             groupBox2.Controls.Add(rbtnInactive);
             groupBox2.Controls.Add(rbtnActive);
-            groupBox2.Location = new Point(393, 38);
+            groupBox2.Location = new Point(449, 51);
+            groupBox2.Margin = new Padding(3, 4, 3, 4);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(335, 129);
+            groupBox2.Padding = new Padding(3, 4, 3, 4);
+            groupBox2.Size = new Size(383, 172);
             groupBox2.TabIndex = 8;
             groupBox2.TabStop = false;
             groupBox2.Text = "Estado";
@@ -150,24 +156,28 @@
             // rbtnInactive
             // 
             rbtnInactive.AutoSize = true;
-            rbtnInactive.Location = new Point(25, 79);
+            rbtnInactive.Location = new Point(29, 105);
+            rbtnInactive.Margin = new Padding(3, 4, 3, 4);
             rbtnInactive.Name = "rbtnInactive";
-            rbtnInactive.Size = new Size(71, 20);
+            rbtnInactive.Size = new Size(87, 24);
             rbtnInactive.TabIndex = 1;
             rbtnInactive.TabStop = true;
             rbtnInactive.Text = "Inactivo";
             rbtnInactive.UseVisualStyleBackColor = true;
+            rbtnInactive.CheckedChanged += rbtnInactive_CheckedChanged;
             // 
             // rbtnActive
             // 
             rbtnActive.AutoSize = true;
-            rbtnActive.Location = new Point(25, 38);
+            rbtnActive.Location = new Point(29, 51);
+            rbtnActive.Margin = new Padding(3, 4, 3, 4);
             rbtnActive.Name = "rbtnActive";
-            rbtnActive.Size = new Size(62, 20);
+            rbtnActive.Size = new Size(76, 24);
             rbtnActive.TabIndex = 0;
             rbtnActive.TabStop = true;
             rbtnActive.Text = "Activo";
             rbtnActive.UseVisualStyleBackColor = true;
+            rbtnActive.CheckedChanged += rbtnActive_CheckedChanged;
             // 
             // groupBox1
             // 
@@ -175,9 +185,11 @@
             groupBox1.Controls.Add(btnAdd);
             groupBox1.Controls.Add(btnModify);
             groupBox1.Controls.Add(btnNew);
-            groupBox1.Location = new Point(41, 305);
+            groupBox1.Location = new Point(47, 407);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(846, 77);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(967, 103);
             groupBox1.TabIndex = 7;
             groupBox1.TabStop = false;
             // 
@@ -185,9 +197,10 @@
             // 
             btnDelete.Image = (Image)resources.GetObject("btnDelete.Image");
             btnDelete.ImageAlign = ContentAlignment.MiddleLeft;
-            btnDelete.Location = new Point(582, 21);
+            btnDelete.Location = new Point(665, 28);
+            btnDelete.Margin = new Padding(3, 4, 3, 4);
             btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(158, 43);
+            btnDelete.Size = new Size(181, 57);
             btnDelete.TabIndex = 3;
             btnDelete.Text = "Eliminar";
             btnDelete.UseVisualStyleBackColor = true;
@@ -197,9 +210,10 @@
             // 
             btnAdd.Image = (Image)resources.GetObject("btnAdd.Image");
             btnAdd.ImageAlign = ContentAlignment.MiddleLeft;
-            btnAdd.Location = new Point(209, 21);
+            btnAdd.Location = new Point(239, 28);
+            btnAdd.Margin = new Padding(3, 4, 3, 4);
             btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(158, 43);
+            btnAdd.Size = new Size(181, 57);
             btnAdd.TabIndex = 2;
             btnAdd.Text = "Agregar";
             btnAdd.UseVisualStyleBackColor = true;
@@ -209,9 +223,10 @@
             // 
             btnModify.Image = (Image)resources.GetObject("btnModify.Image");
             btnModify.ImageAlign = ContentAlignment.MiddleLeft;
-            btnModify.Location = new Point(391, 21);
+            btnModify.Location = new Point(447, 28);
+            btnModify.Margin = new Padding(3, 4, 3, 4);
             btnModify.Name = "btnModify";
-            btnModify.Size = new Size(158, 43);
+            btnModify.Size = new Size(181, 57);
             btnModify.TabIndex = 1;
             btnModify.Text = "Modificar";
             btnModify.UseVisualStyleBackColor = true;
@@ -221,9 +236,10 @@
             // 
             btnNew.Image = (Image)resources.GetObject("btnNew.Image");
             btnNew.ImageAlign = ContentAlignment.MiddleLeft;
-            btnNew.Location = new Point(18, 21);
+            btnNew.Location = new Point(21, 28);
+            btnNew.Margin = new Padding(3, 4, 3, 4);
             btnNew.Name = "btnNew";
-            btnNew.Size = new Size(158, 43);
+            btnNew.Size = new Size(181, 57);
             btnNew.TabIndex = 0;
             btnNew.Text = "Nuevo";
             btnNew.UseVisualStyleBackColor = true;
@@ -232,18 +248,19 @@
             // txtCamion
             // 
             txtCamion.Font = new Font("Microsoft Sans Serif", 9.75F);
-            txtCamion.Location = new Point(41, 145);
+            txtCamion.Location = new Point(47, 193);
+            txtCamion.Margin = new Padding(3, 4, 3, 4);
             txtCamion.Name = "txtCamion";
-            txtCamion.Size = new Size(289, 22);
+            txtCamion.Size = new Size(330, 26);
             txtCamion.TabIndex = 3;
             // 
             // lblZona
             // 
             lblZona.AutoSize = true;
             lblZona.Font = new Font("Microsoft Sans Serif", 10F);
-            lblZona.Location = new Point(41, 117);
+            lblZona.Location = new Point(47, 156);
             lblZona.Name = "lblZona";
-            lblZona.Size = new Size(47, 17);
+            lblZona.Size = new Size(56, 20);
             lblZona.TabIndex = 2;
             lblZona.Text = "Marca";
             // 
@@ -251,18 +268,19 @@
             // 
             txtCodeCamion.Enabled = false;
             txtCodeCamion.Font = new Font("Microsoft Sans Serif", 9.75F);
-            txtCodeCamion.Location = new Point(41, 76);
+            txtCodeCamion.Location = new Point(47, 101);
+            txtCodeCamion.Margin = new Padding(3, 4, 3, 4);
             txtCodeCamion.Name = "txtCodeCamion";
-            txtCodeCamion.Size = new Size(292, 22);
+            txtCodeCamion.Size = new Size(333, 26);
             txtCodeCamion.TabIndex = 1;
             // 
             // lblCode
             // 
             lblCode.AutoSize = true;
             lblCode.Font = new Font("Microsoft Sans Serif", 10F);
-            lblCode.Location = new Point(41, 38);
+            lblCode.Location = new Point(47, 51);
             lblCode.Name = "lblCode";
-            lblCode.Size = new Size(118, 17);
+            lblCode.Size = new Size(141, 20);
             lblCode.TabIndex = 0;
             lblCode.Text = "Código Camiones";
             // 
@@ -273,22 +291,25 @@
             gvCamiones.AllowUserToOrderColumns = true;
             gvCamiones.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             gvCamiones.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            gvCamiones.Location = new Point(12, 473);
+            gvCamiones.Location = new Point(14, 631);
+            gvCamiones.Margin = new Padding(3, 4, 3, 4);
             gvCamiones.Name = "gvCamiones";
-            gvCamiones.Size = new Size(905, 172);
+            gvCamiones.RowHeadersWidth = 51;
+            gvCamiones.Size = new Size(1034, 229);
             gvCamiones.TabIndex = 5;
             gvCamiones.CellContentClick += gvCamiones_CellContentClick;
             // 
             // Camiones
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1103, 729);
+            ClientSize = new Size(1261, 972);
             Controls.Add(gvCamiones);
             Controls.Add(gbRaza);
             Controls.Add(label1);
             Controls.Add(label2);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Camiones";
             Text = "Camiones";
             Load += Camiones_Load;
--- InventZetaGas/Camiones.cs
+++ InventZetaGas/Camiones.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaNegocios;
+using CapaEntidades;
 
 namespace InventZetaGas
 {
@@ -17,39 +19,134 @@ namespace InventZetaGas
             InitializeComponent();
         }
 
+        CamionesN camionN = new CamionesN();
+        CamionesE camionE = new CamionesE();
+        Generales g = new Generales();
+        private DataView dataView;
+
+
         #region Metodos del formulario
         private void Camiones_Load(object sender, EventArgs e)
         {
-
+            CargarDatos();
         }
 
         private void btnNew_Click(object sender, EventArgs e)
         {
-
+            Limpiar();
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            if (string.IsNullOrEmpty(txtCamion.Text))
+            {
+                MessageBox.Show("Campos sin completar, por favor llenar los datos", "Campos vacíos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (string.IsNullOrEmpty(txtCodeCamion.Text))
+            {
+                // Pregunta si desea registrar el siguiente dato.
+                if (MessageBox.Show($"¿Deseas registrar a {txtCamion.Text}?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    // Método para realizar el insert en SQL con la acción "1".
+                    Mantenimiento("1");
+                    Limpiar();
+                }
+            }
         }
 
         private void btnModify_Click(object sender, EventArgs e)
         {
-
+            // Pregunta si desea modificar el dato.
+            if (MessageBox.Show($"¿Deseas modificar {txtCamion.Text}?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                Mantenimiento("2");
+                Limpiar();
+            }
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-
+            if (MessageBox.Show($"¿Deseas eliminar {txtCamion.Text}?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                Mantenimiento("3");
+                Limpiar();
+            }
         }
 
         private void gvCamiones_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            if (e.RowIndex >= 0)
+            {
+                // Obtén la fila seleccionada
+                DataGridViewRow row = gvCamiones.Rows[e.RowIndex];
+                // Asigna los valores de las celdas a los TextBox
+                txtCodeCamion.Text = row.Cells["Zona ID"].Value?.ToString();
+                txtCamion.Text = row.Cells["Nombre Zona"].Value?.ToString();
+                txtPesaje.Text = row.Cells["Provincia"].Value?.ToString();
+                txtPlaca.Text = row.Cells["Provincia"].Value?.ToString();
+            }
+        }
 
+
+        private void rbtnActive_CheckedChanged(object sender, EventArgs e)
+        {
+            Estados();
+        }
+
+        private void rbtnInactive_CheckedChanged(object sender, EventArgs e)
+        {
+            Estados();
         }
         #endregion
 
         #region MetodosGenerales
+        //metodo para cargar la lista de camiones
+        private void CargarDatos()
+        {
+            gvCamiones.DataSource = camionN.ListaCamion();
+        }
+        //************************************************
+        //metodo para limpiar los campos
+        public void Limpiar()
+        {
+            txtCodeCamion.Text = "";
+            txtCamion.Text = "";
+            txtPesaje.Text = "";
+            txtPlaca.Text = "";
+            rbtnActive.Checked = false;
+            rbtnInactive.Checked = false;
+            CargarDatos();
+        }
+
+        // metodo para seleccionar los radio button sea activo o inactivo
+        public void Estados()
+        {
+            if (rbtnActive.Checked)
+            {
+                rbtnInactive.Checked = false;
+                camionE.CamionStatus = rbtnActive.Checked;
+            }
+            else if (rbtnInactive.Checked)
+            {
+                rbtnActive.Checked = false;
+                camionE.CamionStatus = rbtnInactive.Checked;
+            }
+        }
+
+        private void Mantenimiento(string accion)
+        {
+            camionE.CamionCode = txtCodeCamion.Text;
+            camionE.CamionPlaca = txtPlaca.Text;
+            camionE.CamionName = txtCamion.Text;
+            camionE.CamionPesaje = txtPesaje.Text;
+            g.accion = accion;
+            g.msj = camionN.MantenimientoCamiones(camionE, g.accion);
+            MessageBox.Show(g.msj, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        //metodo de mantenimiento de Camiones
+
         #endregion
+
+
     }
 }
