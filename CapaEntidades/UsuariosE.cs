b7026290415994ce4f79a3e2e43e4c8e190479ab
DanielRojasF <87210796+DanielRojasF@users.noreply.github.com>
2025-02-02 18:18:34 -0600

    Se añaden capas de Usuarios, capas de Negocio, Datos y Entidades
    Las capas aun no estan terminadas, faltan de hacer modificaciones

---
+++ CapaEntidades/UsuariosE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class UsuariosE
+    {
+        public int UsuarioID { get; set; }
+        public string UsuarioCode { get; set; }
+        public string UsuarioName { get; set; }
+        public DateTime UsuarioCreacion { get; set; }
+        public DateTime UsuarioUpdate { get; set; }
+        public DateTime UsuarioDelete { get; set; }
+        public bool UsuarioStatus { get; set; }
+    }
+}
---
+++ CapaNegocios/UsuariosN.cs
@@ -0,0 +1,21 @@
+﻿using CapaDatos;
+using CapaEntidades;
+using System.Data;
+
+namespace CapaNegocios
+{
+    public  class UsuariosN
+    {
+        UsuariosD UsuariosD = new UsuariosD();
+
+        public string MantenimientoUsuarios(UsuariosE Usuario, string accion)
+        {
+            return UsuariosD.MantenimientoUsuarios(Usuario, accion);
+        }
+
+        public DataTable ListaUsuario()
+        {
+            return UsuariosD.ListarUsuarios();
+        }
+    }
+}
