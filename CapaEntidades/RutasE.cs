85459ef2ad1e0aa120781d01d62c6e75fdfb59c2
andreyszccr <andreyszcr@gmail.com>
2025-02-09 14:51:18 -0600

    Clase de Rutas

---
+++ CapaEntidades/RutasE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class RutasE
+    {
+        public int IdRuta { get; set; }
+        public string RutaCode { get; set; }
+        public string Ruta { get; set; }
+        public bool RutaStatus { get; set; }
+    }
+}
