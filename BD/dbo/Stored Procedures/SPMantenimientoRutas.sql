48b9b63a5e2b6b57ca8afde5c52400ccedc03cac
andreyszccr <andreyszcr@gmail.com>
2025-02-09 21:08:55 -0600

    Modificacion de tablas y store procedure de Rutas

---
+++ BD/dbo/Stored Procedures/SPMantenimientoRutas.sql
@@ -0,0 +1,42 @@
+﻿
+
+
+
+--exec SPMantenimientoRutas '','35',1,'1'
+
+--select * from  Rutas
+
+CREATE PROCEDURE dbo.SPMantenimientoRutas
+(
+	@RutaCode	varchar(5)	,
+	@Ruta		varchar(100),
+	@RutaStatus bit			,
+    @accion VARCHAR(50) OUTPUT
+)
+AS
+BEGIN
+    IF (@accion = '1') -- Insertar nueva provincia
+    BEGIN
+	
+		declare @codnuevo varchar(5), @codmax varchar(5)
+		set @codmax = (select max(RutaCode) from Rutas)
+		set @codmax = isnull(@codmax,'C0000')
+		set @codnuevo = 'C'+RIGHT(RIGHT(@codmax,4)+10001,4)
+
+        INSERT INTO Rutas 
+		( 
+		 RutaCode	, 
+		 Ruta		,
+		 RutaStatus
+		)
+        VALUES 
+		(
+		@codnuevo	,  
+		@Ruta		,
+		@RutaStatus	
+		);
+
+        SET @accion = 'Se generó la ruta '+@Ruta;
+		print @accion;
+    END
+END;
--- BD/dbo/Tables/Roles.sql
+++ BD/dbo/Tables/Roles.sql
@@ -2,7 +2,9 @@
     [RolesId]    INT           IDENTITY (1, 1) NOT NULL,
     [RoleCode]   VARCHAR (5)   NOT NULL,
     [RoleName]   VARCHAR (100) NOT NULL,
-    [RoleStatus] BIT           NOT NULL,
+    [RoleStatus] BIT           NULL,
     CONSTRAINT [PK__Roles__C4B27840F134CA87] PRIMARY KEY CLUSTERED ([RolesId] ASC)
 );
+
+
 
