c13fe07b13e423b8a6a75ba9692844f94a8263cc
andreyszccr <andreyszcr@gmail.com>
2025-02-09 23:27:56 -0600

    Mantenimiento de Rutas

--- CapaDatos/RutasD.cs
+++ CapaDatos/RutasD.cs
@@ -1,16 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using CapaDatos.SQL;
+using CapaEntidades;
+
 
 namespace CapaDatos
 {
     public class RutasD
     {
-        public int IdRuta { get; set; }
-        public string RutaCode { get; set; }
-        public string Ruta { get; set; }
-        public bool RutaStatus { get; set; }
+
+        private readonly ConexionDB _conexion = new ConexionDB();
+
+        #region "Mantenimiento de Zonas"
+        public string MantenimientoRutas(RutasE rutas, string accion)
+        {
+            try
+            {
+
+                Generales g = new Generales();
+                g.accion = accion;
+                using (var cmd = new SqlCommand("SPMantenimientoZonas", _conexion.AbrirConexion()))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@RutaCode", rutas.RutaCode);
+                    cmd.Parameters.AddWithValue("@Ruta", rutas.Ruta);
+                    cmd.Parameters.AddWithValue("@@RutaStatus", rutas.RutaStatus);
+                    cmd.Parameters.Add("@accion", SqlDbType.VarChar, 50).Value = g.accion;
+                    cmd.Parameters["@accion"].Direction = ParameterDirection.InputOutput;
+                    cmd.ExecuteNonQuery();
+                    _conexion.CerrarConexion();
+                    return cmd.Parameters["@accion"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                var value = ex.GetType().ToString();
+                return value;
+            }
+        }
+        #endregion
     }
 }
