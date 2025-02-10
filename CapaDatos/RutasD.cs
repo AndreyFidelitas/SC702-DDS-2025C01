c84c0b4c34322409febb805fd118c952a121a6dc
andreyszccr <andreyszcr@gmail.com>
2025-02-09 19:03:34 -0600

    Clase de Rutas

---
+++ CapaDatos/RutasD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class RutasD
+    {
+        public int IdRuta { get; set; }
+        public string RutaCode { get; set; }
+        public string Ruta { get; set; }
+        public bool RutaStatus { get; set; }
+    }
+}
