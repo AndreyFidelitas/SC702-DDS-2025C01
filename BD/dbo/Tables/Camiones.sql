2b64e8ebf3b37bc0b55503bafb1db6110c52bfff
andreyszccr <andreyszcr@gmail.com>
2025-02-13 00:06:42 -0600

    Ajuste de bug en tabla de Camiones

--- BD/dbo/Tables/Camiones.sql
+++ BD/dbo/Tables/Camiones.sql
@@ -7,7 +7,9 @@
     [CamionCreacion] DATETIME      NULL,
     [CamionUpdate]   DATETIME      NULL,
     [CamionDelete]   DATETIME      NULL,
-    [CamionStatus]   INT           NULL,
+    [CamionStatus]   BIT           NULL,
     CONSTRAINT [PK__Camiones__80E86F48460A709E] PRIMARY KEY CLUSTERED ([CamionID] ASC)
 );
+
+
 
