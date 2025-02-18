9e2d40ee06c3179dd5ec2e1b16136fa6f3bc0307
andreyszccr <andreyszcr@gmail.com>
2025-02-17 20:58:46 -0600

    Store procedure nuevo de clientes y clase de Clientes y creacion de CapaDatos de Cliente

---
+++ CapaEntidades/ClientesE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class ClientesE
+    {
+        public int ClientesID { get; set; }
+        public string ClientesCode { get; set; }
+        public string RazonSocial { get; set; }
+        public string Empresa { get; set; }
+        public string ClientesRol { get; set; }
+        public DateTime ClientesCreacion { get; set; }
+        public DateTime ClientesUpdate { get; set; }
+        public DateTime ClientesDelete { get; set; }
+        public bool ClientesStatus { get; set; }
+    }
+}
