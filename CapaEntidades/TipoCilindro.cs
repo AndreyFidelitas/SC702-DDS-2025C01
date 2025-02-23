6c00961225c5ab75fe9ac6529df50acb3288dc8e
Clifford <123217596+Cliff371@users.noreply.github.com>
2025-02-22 18:40:10 -0600

    Clase de tipos de cilindro

---
+++ CapaEntidades/TipoCilindro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class TipoCilindro
+    {
+        public int TipoCilindroID { get; set; }
+        public string TipoCilindroCode { get; set; }
+        public string LoteLitraje { get; set; }
+        public bool TipoCilindroStatus { get; set; }
+    }
+}
