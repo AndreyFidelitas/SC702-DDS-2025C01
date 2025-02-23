0f4431f81d4567e1e0dc12bc00e66f20068da612
andreyszccr <andreyszcr@gmail.com>
2025-02-22 18:55:29 -0600

    Capa Negocio de Clientes

---
+++ CapaNegocios/ClientesN.cs
@@ -0,0 +1,22 @@
+﻿using CapaDatos;
+using CapaEntidades;
+using System.Data;
+
+namespace CapaNegocios
+{
+    public class ClientesN
+    {
+        ClientesD ClientesD = new ClientesD();
+
+        public string MantenimientoClientes(ClientesE Clientes, string accion)
+        {
+            //return Clientes. (Clientes, accion);
+            return ClientesD.MantenimientoClientes(Clientes, accion);
+        }
+
+        public DataTable ListaClientes()
+        {
+            return ClientesD.ListarClientes();
+        }
+    }
+}
