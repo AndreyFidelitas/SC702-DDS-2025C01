1f2d165e72dca36b777534d8bf3af1c81e5ff91e
andreyszccr <andreyszcr@gmail.com>
2025-03-03 17:13:58 -0600

    Implementacion de Api de ClientesEmpresarial y modulos para obtener la informacion

---
+++ CapaEntidades/ApiEmpresarial/Situacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades.ApiEmpresarial
+{
+    public class Situacion
+    {
+        public string Moroso { get; set; }
+        public string Omiso { get; set; }
+        public string Estado { get; set; }
+        public string AdministracionTributaria { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
