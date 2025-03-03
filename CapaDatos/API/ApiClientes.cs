0681a70efc3beecd45f224236417d13a81e369b3
andreyszccr <andreyszcr@gmail.com>
2025-03-02 23:02:56 -0600

    integracion de API y de Modelos para consultar al API de clientes

---
+++ CapaDatos/API/ApiClientes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CapaDatos.API
+{
+    public class ApiClientes
+    {
+
+    }
+}
---
+++ CapaEntidades/ApiResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class ApiResponse
+    {
+        public string Query { get; set; }
+        public string Cedula { get; set; }
+        public string User { get; set; }
+        public List<Result> Results { get; set; }
+        public string DatabaseDate { get; set; }
+        public string TipoIdentificacion { get; set; }
+        public string License { get; set; }
+        public int Resultcount { get; set; }
+        public string Nombre { get; set; }
+    }
+}
