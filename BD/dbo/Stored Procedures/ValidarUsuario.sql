578160fba22e0196d2907661d97534960faa6963
andreyszccr <andreyszcr@gmail.com>
2025-03-10 18:41:49 -0600

    Cambios en los store procedures y relacion de tablas

---
+++ BD/dbo/Stored Procedures/ValidarUsuario.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE ValidarUsuario
+    @Cedula INT = NULL,  -- Parámetro opcional para buscar un usuario específico
+    @UsuarioUserName varchar(100) = NULL -- Parámetro opcional para buscar por cédula
+AS
+BEGIN
+    SELECT 
+        u.UsuarioCode											AS [Usuario ID],
+        u.Cedula												AS [Cedula],
+        u.UsuarioName											AS [Nombre],
+        u.UsuarioApellidos										AS [Apellidos],
+        u.UsuarioUserName										AS [Nombre de Usuario],
+        r.RoleName												AS [ID Rol],
+		u.Password												as [Contraseña],
+        ISNULL(CONVERT(VARCHAR, u.UsuarioCreacion, 13), '-')	AS [Fecha Creación],
+        ISNULL(CONVERT(VARCHAR, u.UsuarioDelete, 13), '-')		AS [Fecha Eliminado],
+        ISNULL(CONVERT(VARCHAR, u.UsuarioUpdate, 13), '-')		AS [Fecha Modificado],
+        CASE 
+            WHEN u.UsuarioEstado = 1 THEN 'Activo'
+            ELSE 'Inactivo'
+        END                                                 AS [Estado]
+    FROM 
+        Usuarios u WITH (NOLOCK)
+    INNER JOIN 
+        Roles r WITH (NOLOCK) ON u.RoleID = r.RolesId
+    WHERE 
+        (@UsuarioUserName IS NULL OR u.UsuarioUserName = @UsuarioUserName)  -- Filtro por UsuarioCode (opcional)
+        AND (@Cedula IS NULL OR u.Cedula = @Cedula);						-- Filtro por Cédula (opcional)
+END;
--- BD/dbo/Tables/InventarioDetalle.sql
+++ BD/dbo/Tables/InventarioDetalle.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[InventarioDetalle] (
     [InventarioDetalleID]   INT         IDENTITY (1, 1) NOT NULL,
+    [InventarioID]          INT         NULL,
     [InventarioDetalleCode] VARCHAR (5) NULL,
     [TipoCilindroID]        INT         NOT NULL,
     [Cantidad]              INT         NOT NULL,
@@ -9,6 +10,8 @@
     CONSTRAINT [PK__Inventar__709142AA016989AB] PRIMARY KEY CLUSTERED ([InventarioDetalleID] ASC),
     CONSTRAINT [FK_InventarioDetalle_TipoCilindro] FOREIGN KEY ([TipoCilindroID]) REFERENCES [dbo].[TipoCilindro] ([TipoCilindroID])
 );
+
+
 
 
 
