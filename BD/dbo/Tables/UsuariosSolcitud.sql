9575138d060206bc189c559e482282c8925530b0
andreyszccr <andreyszcr@gmail.com>
2025-03-23 19:39:08 -0600

    Tabla de asignacion de creacion de ususarios

---
+++ BD/dbo/Tables/UsuariosSolcitud.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[UsuariosSolcitud] (
+    [SolicitudID]      INT           IDENTITY (1, 1) NOT NULL,
+    [Cedula]           INT           NULL,
+    [Name]             VARCHAR (100) NULL,
+    [Apellidos]        VARCHAR (100) NULL,
+    [SolcitudAceptada] DATETIME      NULL,
+    [SolcitudRechaza]  DATETIME      NULL,
+    [SolcitudEstado]   BIT           NULL,
+    [UsuarioID]        INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([SolicitudID] ASC),
+    CONSTRAINT [FK_UsuariosSolcitud_Usuarios] FOREIGN KEY ([UsuarioID]) REFERENCES [dbo].[Usuarios] ([UsuarioID])
+);
+
