4769e1a022e96ecaa73960b9a3b4155d0a08a9bd
andreyszccr <andreyszcr@gmail.com>
2025-03-25 21:27:26 -0600

    modificacion de tabla de usuarios de Solicitudes y creacion de mantenimiento de solicitud

--- BD/dbo/Tables/UsuariosSolcitud.sql
+++ BD/dbo/Tables/UsuariosSolcitud.sql
@@ -1,13 +1,14 @@
 ﻿CREATE TABLE [dbo].[UsuariosSolcitud] (
     [SolicitudID]      INT           IDENTITY (1, 1) NOT NULL,
-    [Cedula]           INT           NULL,
-    [Name]             VARCHAR (100) NULL,
-    [Apellidos]        VARCHAR (100) NULL,
+    [SolicitudCode]    VARCHAR (5)   NULL,
+    [Cedula]           INT           NOT NULL,
+    [Name]             VARCHAR (100) NOT NULL,
+    [Apellidos]        VARCHAR (100) NOT NULL,
     [SolcitudAceptada] DATETIME      NULL,
     [SolcitudRechaza]  DATETIME      NULL,
     [SolcitudEstado]   BIT           NULL,
-    [UsuarioID]        INT           NOT NULL,
-    PRIMARY KEY CLUSTERED ([SolicitudID] ASC),
+    [UsuarioID]        INT           NULL,
+    CONSTRAINT [PK__Usuarios__85E95DA7BB484BBC] PRIMARY KEY CLUSTERED ([SolicitudID] ASC),
     CONSTRAINT [FK_UsuariosSolcitud_Usuarios] FOREIGN KEY ([UsuarioID]) REFERENCES [dbo].[Usuarios] ([UsuarioID])
 );
 
