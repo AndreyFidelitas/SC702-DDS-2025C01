386f2a2ea10284f8b1d50fab127b76a7b4905992
andreyszccr <andreyszcr@gmail.com>
2025-04-05 17:17:13 -0600

    Imagenes de aceptar y rechazar.
    Formulario de Aceptacion de solicitudes.

--- InventZetaGas/UsuariosSolicitudes.cs
+++ InventZetaGas/UsuariosSolicitudes.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaEntidades;
+using CapaNegocios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,9 @@ namespace InventZetaGas
 {
     public partial class UsuariosSolicitudes : Form
     {
+        RolesN RolesN = new RolesN();
+        Generales g = new Generales();
+        private DataView dataView;
         public UsuariosSolicitudes()
         {
             InitializeComponent();
@@ -19,7 +24,34 @@ namespace InventZetaGas
 
         private void UsuariosSolicitudes_Load(object sender, EventArgs e)
         {
+            CargarListaRoles();
+            cbRol.SelectedIndex = -1;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
 
         }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRechazar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        #region Metodos Generales
+        //metodo para cargar las provincias
+        public void CargarListaRoles()
+        {
+            cbRol.DataSource = RolesN.CargarRoles();
+            cbRol.DisplayMember = "Rol";
+            cbRol.ValueMember = "Codigo Rol";
+        }
+        #endregion
     }
 }
