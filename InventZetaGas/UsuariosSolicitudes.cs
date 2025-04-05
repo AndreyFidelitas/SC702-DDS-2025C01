2f4bd2d0ba4af5f181ee3e02b4d6f89c04f3d883
andreyszccr <andreyszcr@gmail.com>
2025-04-05 16:44:06 -0600

    Diseno de Solicitudes de usuario

---
+++ InventZetaGas/UsuariosSolicitudes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventZetaGas
+{
+    public partial class UsuariosSolicitudes : Form
+    {
+        public UsuariosSolicitudes()
+        {
+            InitializeComponent();
+        }
+
+        private void UsuariosSolicitudes_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
