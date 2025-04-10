b8c74f1a2e43243dc6094d7dca51e8bd87dc6654
andreyszccr <andreyszcr@gmail.com>
2025-04-09 23:57:46 -0600

    Store procedure de lista de usuarios

---
+++ BD/dbo/Stored Procedures/SPListaSolicitudUsuarios.sql
@@ -0,0 +1,20 @@
+﻿create PROCEDURE [dbo].[SPListaSolicitudUsuarios]
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        u.SolicitudCode											[Solicitud Code]	,
+        u.Cedula												[Cedula]			,
+		u.Name													[Nombre]			,
+        u.Apellidos												[Apellidos]			,
+        ISNULL(CONVERT(VARCHAR, u.SolcitudAceptada, 13),'-')	[Fecha Aceptado]	,
+        ISNULL(CONVERT(VARCHAR, u.SolcitudRechaza, 13),'-')		[Fecha Rechazado]	,
+        CASE 
+            WHEN u.SolcitudEstado = 0 THEN 'Rechazado'
+            WHEN u.SolcitudEstado = 1 THEN 'Aceptado'      
+			ELSE '-'
+        END                                                 [Estado]
+    FROM 
+        UsuariosSolcitud	u WITH (NOLOCK)
+END
