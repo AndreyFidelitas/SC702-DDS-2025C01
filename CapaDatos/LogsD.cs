f2a96563289a131a11ea20701766b50776955441
andreyszccr <andreyszcr@gmail.com>
2025-06-17 10:24:14 -0600

    Cambios en Logs de capa de datos y creacion de entity de LogsE

--- CapaDatos/LogsD.cs
+++ CapaDatos/LogsD.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CapaDatos.SQL;
+using CapaEntidades;
+using System.Data.SqlClient;
+using System.Data;
+
 
 namespace CapaDatos
 {
     public class LogsD
     {
+        private readonly ConexionDB _conexion = new ConexionDB();
+
+        #region "Mantenimiento de RolesE"
+        [Obsolete]
+        public string MantenimientoLogs(LogsE LogsE, string accion)
+        {
+            try
+            {
+                Generales g = new Generales();
+                g.accion = accion;
+                using (var cmd = new SqlCommand("SPMantenimientoRoles", _conexion.AbrirConexion()))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@UsuarioID", LogsE.UsuarioID);
+                    cmd.Parameters.AddWithValue("@TablaAfectada", LogsE.TablaAfectada);
+                    cmd.Parameters.AddWithValue("@Modulo", LogsE.Modulo);
+                    cmd.Parameters.Add("@accion", SqlDbType.VarChar, 50).Value = g.accion;
+                    cmd.Parameters["@accion"].Direction = ParameterDirection.InputOutput;
+                    cmd.ExecuteNonQuery();
+                    _conexion.CerrarConexion();
+                    return cmd.Parameters["@accion"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                var value = ex.GetType().ToString();
+                return value;
+            }
+        }
+        #endregion
     }
 }
---
+++ CapaEntidades/LogsE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class LogsE
+    {
+       public int LogID { get; set; }
+       public DateTime FechaEvento { get; set; }
+       public int UsuarioID { get; set; }
+       public string TablaAfectada { get; set; }
+       public string Modulo { get; set; }
+       public string Detalles { get; set; }
+    }
+}
