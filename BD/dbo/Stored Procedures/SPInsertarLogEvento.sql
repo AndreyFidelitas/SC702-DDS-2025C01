6674eae5b1d3964b7635c2ac266116aeffbb2120
andreyszccr <andreyszcr@gmail.com>
2025-06-17 09:48:16 -0600

    Store procedure de logs y capa de datos de logs

---
+++ BD/dbo/Stored Procedures/SPInsertarLogEvento.sql
@@ -0,0 +1,44 @@
+﻿
+
+
+/*
+select * from LogEventos
+
+EXEC [dbo].[SPInsertarLogEvento]
+    @UsuarioID = 11,
+    @TablaAfectada = 'Roles',
+    @Modulo = 'MantenimientoRoles',
+    @Detalles = 'Se insertó un nuevo rol llamado Administrador',
+    @accion = '1';
+*/
+
+CREATE PROCEDURE [dbo].[SPInsertarLogEvento]
+(
+    @UsuarioID INT,
+    @TablaAfectada VARCHAR(100),
+    @Modulo VARCHAR(100),
+    @Detalles NVARCHAR(MAX),
+	@accion VARCHAR(50) OUTPUT      -- Acción a realizar (1 = Insertar, 2 = Actualizar, 3 = Eliminar)
+)
+AS
+BEGIN
+   IF (@accion = '1') -- Insertar nuevo rol
+    BEGIN	
+	
+	SET NOCOUNT ON;
+
+    INSERT INTO LogEventos 
+	(
+        UsuarioID,
+        TablaAfectada,
+        Modulo,
+        Detalles
+    )
+    VALUES (
+        @UsuarioID,
+        @TablaAfectada,
+        @Modulo,
+        @Detalles
+    );
+    END
+END
